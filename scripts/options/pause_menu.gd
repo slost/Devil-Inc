896e84cdca813d59cff0cb5fd3813207ca05090a
UdomUdom <udomzaark@gmail.com>
2024-12-01 05:51:39 +0700

    update pause menu

---
+++ scripts/options/pause_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@onready var pasue_button = $Button_pause as Button
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
+
+func _process(delta):
+	testEsc()
