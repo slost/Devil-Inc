f2ed6b757410904a035903f7047a7515ce358869
slost <slowslost@gmail.com>
2024-12-01 22:46:12 +0700

    Add chest animation

--- scripts/classes/lootbox.gd
+++ scripts/classes/lootbox.gd
@@ -4,6 +4,8 @@ class_name LootBox
 # Time between item spawns
 @export var spawn_time: float = 5.0
 @export var time_to_spawn: float = 30.0
+@export var progress_bar: ProgressBar
+@export var anim_player: AnimationPlayer
 
 # Timer counter
 var spawn_timer: float = 0.0
@@ -12,12 +14,23 @@ var spawn_timer: float = 0.0
 
 func _process(delta: float) -> void:
 
-	label.text = "Next drop: " + str(time_to_spawn - spawn_timer)
+	label.text = "%1.2fs" % (time_to_spawn - spawn_timer)
 
 
 	spawn_timer += delta
 	if spawn_timer >= time_to_spawn:
+		open_chest()
 		spawn_timer = 0
 		Util.generate_item()
 		print("Item spawned")
+
+
+	update_progress_bar()
+
+func update_progress_bar() -> void:
+	progress_bar.value = spawn_timer
+	progress_bar.max_value = time_to_spawn
 		
+func open_chest() -> void:
+	Audio.play_sound(Audio.SFX.OPENING_CHEST)
+	anim_player.play("opening")
